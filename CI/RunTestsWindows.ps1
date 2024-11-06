7f4c19246e0dfe911dd5e81058dc88b16c3b71de
YuraYarmola <uarmola1@gmail.com>
2024-11-07 00:58:27 +0200

    fix script

--- CI/RunTestsWindows.ps1
+++ CI/RunTestsWindows.ps1
@@ -9,20 +9,27 @@ function Is-ComPortConnected($portName) {
         return $false
     }
 }
-if ($env:GITHUB_ACTIONS -eq 'true') {
-    $PythonPath = "$env:pythonLocation\python.exe"
 
+# Set Python path dynamically
+if ($IsGitHubActions) {
+    $PythonPath = "${env:pythonLocation}\python.exe"
 } else {
-    $PythonPath = "D:\LPNU\SEMESTER7\APKS\csad2425ki406yarmolayy29\TicTacToeSWPart\.venv\Scripts\python.exe"
+    $PythonPath = (Join-Path -Path (Get-Location) -ChildPath "..\TicTacToeSWPart\.venv\Scripts\python.exe")
 }
 
+# Move to the project directory
 Set-Location -Path (Join-Path -Path $PSScriptRoot -ChildPath "..\TicTacToeSWPart")
+
 $SkipHwTests = $IsGitHubActions -or !(Is-ComPortConnected -portName $ComPort)
-Write-Output "Running software tests..."
-& $PythonPath -m unittest "sw_tests.py"
+
+Write-Output "Running software tests with pytest for detailed report and coverage..."
+
+# Run software tests with coverage
+& $PythonPath -m pytest --cov=main --cov-report=xml --cov-report=term-missing --junitxml=test-reports/results.xml "sw_tests.py"
+
 if (-not $SkipHwTests) {
     Write-Output "COM port $ComPort detected. Running hardware tests..."
-    & $PythonPath -m unittest "hw_tests.py"
+    & $PythonPath -m pytest --cov=main --cov-append --junitxml=test-reports/hw_results.xml "hw_tests.py"
 } else {
     Write-Output "Skipping hardware tests."
 }
